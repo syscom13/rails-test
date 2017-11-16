26b8919b8c000c980e2c0fc9fd52e0f8bc58b943
Nicolas Verger <nicolas.india@gmail.com>
2017-11-16 14:23:24 +0100

    Add model Product

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
   def home
+    @today = Date.today
+  end
+
+  def team
+    @team = ["Nicolas", "Romain", "Sebastien"]
+  end
+
+  def about
   end
 end
