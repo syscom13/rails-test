659528cf102ceed904d2abde4e3c4faa037d530d
Nicolas Verger <nicolas.india@gmail.com>
2017-11-16 18:01:56 +0100

    Add more products to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,20 @@
 Product.destroy_all
 
-Product.create(name: "Stripe", url: "http://stripe.com", tagline: "Payment integration and management")
-Product.create(name: "Le Wagon", url: "http://lewagon.com", tagline: "Le Wagon brings technical skills to creative people")
-Product.create(name: "Hunter", url: "http://hunter.io", tagline: "Find all the email addresses related to a domain")
+Product.create(name: "Stripe", url: "http://stripe.com",
+                               tagline: "Payment integration and management",
+                               category: "tech")
+Product.create(name: "Le Wagon", url: "http://lewagon.com",
+                                 tagline: "Le Wagon brings technical skills to creative people",
+                                 category: "education")
+Product.create(name: "Hunter", url: "http://hunter.io",
+                               tagline: "Find all the email addresses related to a domain",
+                               category: "tech")
+Product.create(name: "Station", url: "http://getstation.com",
+                               tagline: "One app to rule them all",
+                               category: "tech")
+Product.create(name: "Hunter", url: "http://www.invisionapp.com",
+                               tagline: "Prototyping and collaboration for design teams",
+                               category: "design")
+Product.create(name: "Sketch", url: "http://sketchapp.com",
+                               tagline: "A vector drawing tool for modern designers",
+                               category: "design")
